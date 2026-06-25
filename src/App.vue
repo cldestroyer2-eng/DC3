9c8f44b36da7bb0483fcf8c30cb12550ea1dd257
cldestroyer2@gmail.com <cldestroyer2@gmail.com>
2026-06-25 13:24:38 +0800

    vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Sample from './components/Sample.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <Sample />
 </template>
